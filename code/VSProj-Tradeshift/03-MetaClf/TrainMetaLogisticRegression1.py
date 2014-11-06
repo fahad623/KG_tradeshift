eb96ca7e833f93f9347b71ebbc2f62a00925a87d
fahad623 <fahad2191@gmail.com>
2014-11-05 23:24:42 -0600

    a

--- code/VSProj-Tradeshift/03-MetaClf/TrainMetaLogisticRegression1.py
+++ code/VSProj-Tradeshift/03-MetaClf/TrainMetaLogisticRegression1.py
@@ -7,15 +7,14 @@ import os
 import shutil
 
 trainFileY = "..\\..\\..\\data\\trainLabels.csv"
-testFileX = "..\\..\\..\\data\\test.csv"
 
 clfFolder_base1 = "..\\..\\..\\classifier\\RandomForest\\"
 clfFolder_base2 = "..\\..\\..\\classifier\\LinearSVC\\"
-clfMetaFolder = "..\\..\\..\\classifier\\Meta\\"
+clfFolder = "..\\..\\..\\classifier\\Meta\\"
 
 
 def cv_optimize(X_train, Y_train, clf):
-    alpha_range = [0.01, 0.1, 1, 10, 100]
+    alpha_range = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
     param_grid = dict(alpha=alpha_range)
 
     gs = GridSearchCV(clf, param_grid = param_grid, cv = 10, n_jobs = 8, verbose = 3)
@@ -28,27 +27,28 @@ def fit_clf(X_train, Y_train):
     clf, bp, bs = cv_optimize(X_train, Y_train, clf)    
     clf.fit(X_train, Y_train)
     return clf
-	
 
 if __name__ == '__main__':
 	
-	df_train_X1 = pd.read_csv(clfFolder_base1 + "output_base_proba.csv")
-    df_train_X2 = pd.read_csv(clfFolder_base1 + "output_base.csv")
-	df_train_Y = pd.read_csv(trainFileY)
-	yCols = df_train_Y.columns.values.tolist()
-	
-	df_train_output = pd.DataFrame(df_train_Y[['id']])
-    df_train_output_proba = pd.DataFrame(df_train_Y[['id']])
-	
-	for colName in yCols[1:]:
-		
-		Y_train = df_train_Y[colName].values
-        if np.any(Y_train != 0):
+    df_train_X1 = pd.read_csv(clfFolder_base1 + "output_base_proba.csv")
+    df_train_X2 = pd.read_csv(clfFolder_base2 + "output_base.csv")
+    df_train_Y = pd.read_csv(trainFileY)
+    yCols = df_train_Y.columns.values.tolist()
 
-			X_train = np.hstack((df_train_X1[colName].values, df_train_X2[colName].values))
-	
+    df_train_output = pd.DataFrame(df_train_Y[['id']])
+    df_train_output_proba = pd.DataFrame(df_train_Y[['id']])
+
+    for colName in yCols[1:]:
+
+        Y_train = df_train_Y[colName].values
+        if np.any(Y_train != 0):
+            X_train1 = df_train_X1[colName].values
+            X_train2 = df_train_X2[colName].values
+            X_train = np.vstack((X_train1, X_train2)).T
+            print X_train.shape
+            del X_train1, X_train2
             clf = fit_clf(X_train, Y_train)
-			df_train_output[colName] = clf.predict(X_train)
+            df_train_output[colName] = clf.predict(X_train)
             df_train_output_proba[colName] = clf.predict_proba(X_train)[:, 1]
 
             pathClassifier = clfFolder+'classifier_{0}\\'.format(colName)
@@ -63,6 +63,6 @@ if __name__ == '__main__':
         else:
             df_train_output[colName] = 0
             df_train_output_proba[colName] = 0
-	
-	df_train_output.to_csv(clfFolder + "output_meta.csv", index = False)
+
+    df_train_output.to_csv(clfFolder + "output_meta.csv", index = False)
     df_train_output_proba.to_csv(clfFolder + "output_meta_proba.csv", index = False)
--- code/VSProj-Tradeshift/JoinDataFrames.py
+++ code/VSProj-Tradeshift/JoinDataFrames.py
@@ -3,8 +3,11 @@ import pandas as pd
 clfFolder = "..\\..\\classifier\\LinearSVC\\"
 
 df_base1 = pd.read_csv(clfFolder + "output_base_part1.csv")
+print df_base1.shape
 df_base2 = pd.read_csv(clfFolder + "output_base_part2.csv")
+print df_base2.shape
+df_base2.drop(['id'], axis=1, inplace = True)
 
 df_output = df_base1.join(df_base2)
-
+print df_output.shape
 df_output.to_csv(clfFolder + "output_base.csv", index = False)
